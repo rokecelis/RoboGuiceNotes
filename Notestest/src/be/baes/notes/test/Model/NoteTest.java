31bbb45f3be71ea2ce50770c5ac8e778f41677c1
Christiaan Baes <chris.baes@skynet.be>
2011-11-06 09:10:23 +0100

    added more tests for model.

--- Notestest/src/be/baes/notes/test/Model/NoteTest.java
+++ Notestest/src/be/baes/notes/test/Model/NoteTest.java
@@ -10,25 +10,48 @@ public class NoteTest extends TestCase {
 	
 	protected void setUp() throws Exception {
 		super.setUp();
-		note = new Note((Long) 0l,"","");
+		note = new Note(0l,"","");
 	}
 	
 	public void testIfIdCanBeSetAndRetrieved()
 	{
-		note.setId((Long) 1l);
-		Assert.assertEquals((Long)1l, note.getId());
+		note.setId(1l);
+		assertEquals((Long)1l, note.getId());
 	}
 	
 	public void testIfTitleCanBeSetAndRetrieved()
 	{
 		note.setTitle("test");
-		Assert.assertEquals("test", note.getTitle());
+		assertEquals("test", note.getTitle());
 	}
 	
 	public void testIfNoteCanBeSetAndRetrieved()
 	{
 		note.setNote("test");
-		Assert.assertEquals("test", note.getNote());
+		assertEquals("test", note.getNote());
+	}
+	
+	public void testIf2NotesAreEqualIfTheIdIsNotEqual()
+	{
+		Note note2 = new Note(0l,"","");
+		assertTrue(note.equals(note2));
+	}
+	
+	public void testIf2NotesAreNotEqualIfTheIdIsNotEqual()
+	{
+		Note note2 = new Note(1l,"","");
+		assertFalse(note.equals(note2));
+	}
+	
+	public void testIf2NotesAreNotEqualIf1IsNull()
+	{
+		Note note2 = null;
+		assertFalse(note.equals(note2));
+	}
+	
+	public void testIfToStringReturnsCorrectResult()
+	{
+		assertEquals("Note [id=0, title=, note=]",note.toString());
 	}
 
 }
